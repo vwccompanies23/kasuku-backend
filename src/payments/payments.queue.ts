9c2f25d6a37cc0554c6815be23c035ab95858e7a
vwc inc <vwccompanies23@email.com>
2026-05-01 22:51:17 -0400

    fix redis connection

--- src/payments/payments.queue.ts
+++ src/payments/payments.queue.ts
@@ -2,7 +2,6 @@ import { Queue } from 'bullmq';
 
 export const paymentsQueue = new Queue('payments', {
   connection: {
-    host: '127.0.0.1',
-    port: 6379,
+    url: process.env.REDIS_URL,
   },
 });
